d894fd32eb0b75c473abf73aedfa20179599eb46
Bert Lange <bert.lange@gmx.net>
2018-03-03 11:38:27 +0100

    change: handling to support less good programmed joystick software

--- rtl/joystick_emu.vhd
+++ rtl/joystick_emu.vhd
@@ -26,6 +26,7 @@ use ieee.std_logic_1164.all;
 entity joystick_emu is
     port
     (
+        clk             : in  std_logic;
         mode            : in  std_logic_vector( 1 downto 0);
         --
         joystick_0      : in  std_logic_vector( 7 downto 0);
@@ -48,9 +49,17 @@ architecture rtl of joystick_emu is
     constant joy_up         : natural := 3;
     constant joy_fire_a     : natural := 4;
     constant joy_fire_b     : natural := 5;
+    --
+    signal   userport_reg   : std_logic_vector( 7 downto 0);
 
 
 begin
+
+    process
+    begin
+        wait until rising_edge( clk);
+        userport_in <= userport_reg;
+    end process;
         
     process( mode, joystick_0, joystick_1, userport_out)
     begin
@@ -65,48 +74,43 @@ begin
                 -- two sticks, multiplexed
                 -- with sound output
                 -- most common
-                userport_in( 0) <= '1';
-                userport_in( 1) <= '1';
-                userport_in( 2) <= '1';
-                userport_in( 3) <= '1';
-                userport_in( 4) <= '1';
-                -- joystick 0 (left jack)
-                if userport_out( 5) = '0' then
-                    userport_in( 0) <= not joystick_0( joy_left);
-                    userport_in( 1) <= not joystick_0( joy_right);
-                    userport_in( 2) <= not joystick_0( joy_down);
-                    userport_in( 3) <= not joystick_0( joy_up);
-                    userport_in( 4) <= not ( joystick_0( joy_fire_a) or joystick_0( joy_fire_b));
-                end if;
-                -- joystick 1 (right jack)
-                if userport_out( 6) = '0' then
-                    userport_in( 0) <= not joystick_1( joy_left);
-                    userport_in( 1) <= not joystick_1( joy_right);
-                    userport_in( 2) <= not joystick_1( joy_down);
-                    userport_in( 3) <= not joystick_1( joy_up);
-                    userport_in( 4) <= not ( joystick_1( joy_fire_a) or joystick_1( joy_fire_b));
-                end if;
-                userport_in( 5) <= userport_out( 5);
-                userport_in( 6) <= userport_out( 6);
-                userport_in( 7) <= userport_out( 7);
+                case userport_out( 6 downto 5) is
+                    when "10" =>
+                        -- joystick 0 (left jack)
+                        userport_reg( 0) <= not joystick_0( joy_left);
+                        userport_reg( 1) <= not joystick_0( joy_right);
+                        userport_reg( 2) <= not joystick_0( joy_down);
+                        userport_reg( 3) <= not joystick_0( joy_up);
+                        userport_reg( 4) <= not ( joystick_0( joy_fire_a) or joystick_0( joy_fire_b));
+                    when others =>
+                        -- joystick 1 (right jack)
+                        userport_reg( 0) <= not joystick_1( joy_left);
+                        userport_reg( 1) <= not joystick_1( joy_right);
+                        userport_reg( 2) <= not joystick_1( joy_down);
+                        userport_reg( 3) <= not joystick_1( joy_up);
+                        userport_reg( 4) <= not ( joystick_1( joy_fire_a) or joystick_1( joy_fire_b));
+                end case;
+                userport_reg( 5) <= userport_out( 5);
+                userport_reg( 6) <= userport_out( 6);
+                userport_reg( 7) <= userport_out( 7);
                 --
                 sound   <= userport_out( 7);
 
             when "01" =>
                 -- ju+te 6/87 (left or right jack)
                 -- one stick
-                userport_in( 0) <= '1';
-                userport_in( 1) <= '0';
-                userport_in( 2) <= '0';
-                userport_in( 3) <= '0';
-                userport_in( 4) <= not( joystick_0( joy_down)  or joystick_1( joy_down));
-                userport_in( 5) <= not( joystick_0( joy_left)  or joystick_1( joy_left));
-                userport_in( 6) <= not( joystick_0( joy_right) or joystick_1( joy_right));
-                userport_in( 7) <= not( joystick_0( joy_up)    or joystick_1( joy_up));
+                userport_reg( 0) <= '1';
+                userport_reg( 1) <= '0';
+                userport_reg( 2) <= '0';
+                userport_reg( 3) <= '0';
+                userport_reg( 4) <= not( joystick_0( joy_down)  or joystick_1( joy_down));
+                userport_reg( 5) <= not( joystick_0( joy_left)  or joystick_1( joy_left));
+                userport_reg( 6) <= not( joystick_0( joy_right) or joystick_1( joy_right));
+                userport_reg( 7) <= not( joystick_0( joy_up)    or joystick_1( joy_up));
                 -- 'press' all directions same time on fire
                 if joystick_0( joy_fire_a) or joystick_0( joy_fire_b) or
                    joystick_1( joy_fire_a) or joystick_1( joy_fire_b) then
-                    userport_in( 7 downto 4) <= "0000";
+                    userport_reg( 7 downto 4) <= "0000";
                 end if;
                 --
                 sound   <= userport_out( 0);
@@ -118,49 +122,44 @@ begin
                 -- no extra sound
 
                 -- joystick 0 (left jack)
-                userport_in( 7) <= not joystick_0( joy_right);
-                userport_in( 6) <= not joystick_0( joy_down);
-                userport_in( 5) <= not joystick_0( joy_left);
-                userport_in( 4) <= not joystick_0( joy_up);
+                userport_reg( 7) <= not joystick_0( joy_right);
+                userport_reg( 6) <= not joystick_0( joy_down);
+                userport_reg( 5) <= not joystick_0( joy_left);
+                userport_reg( 4) <= not joystick_0( joy_up);
                 if joystick_0( joy_fire_a) or joystick_0( joy_fire_b) then
-                    userport_in( 7 downto 4) <= "0000";
+                    userport_reg( 7 downto 4) <= "0000";
                 end if;
                 -- joystick 1 (right jack)
-                userport_in( 3) <= not joystick_1( joy_right);
-                userport_in( 2) <= not joystick_1( joy_down);
-                userport_in( 1) <= not joystick_1( joy_left);
-                userport_in( 0) <= not joystick_1( joy_up);
+                userport_reg( 3) <= not joystick_1( joy_right);
+                userport_reg( 2) <= not joystick_1( joy_down);
+                userport_reg( 1) <= not joystick_1( joy_left);
+                userport_reg( 0) <= not joystick_1( joy_up);
                 if joystick_1( joy_fire_a) or joystick_1( joy_fire_b) then
-                    userport_in( 3 downto 0) <= "0000";
+                    userport_reg( 3 downto 0) <= "0000";
                 end if;
 
             when "11" =>
                 -- like practic 1/88, but up, down & left are scrambled
                 -- used by unpatched "IG ERF-soft" programs
-                userport_in( 0) <= '1';
-                userport_in( 1) <= '1';
-                userport_in( 2) <= '1';
-                userport_in( 3) <= '1';
-                userport_in( 4) <= '1';
-                -- joystick 0 (left jack)
-                if userport_out( 5) = '0' then
-                    userport_in( 1) <= not joystick_0( joy_right);
-                    userport_in( 3) <= not joystick_0( joy_left);
-                    userport_in( 2) <= not joystick_0( joy_up);
-                    userport_in( 0) <= not joystick_0( joy_down);
-                    userport_in( 4) <= not ( joystick_0( joy_fire_a) or joystick_0( joy_fire_b));
-                end if;
-                -- joystick 1 (right jack)
-                if userport_out( 6) = '0' then
-                    userport_in( 1) <= not joystick_1( joy_right);
-                    userport_in( 3) <= not joystick_1( joy_left);
-                    userport_in( 2) <= not joystick_1( joy_up);
-                    userport_in( 0) <= not joystick_1( joy_down);
-                    userport_in( 4) <= not ( joystick_1( joy_fire_a) or joystick_1( joy_fire_b));
-                end if;
-                userport_in( 5) <= userport_out( 5);
-                userport_in( 6) <= userport_out( 6);
-                userport_in( 7) <= userport_out( 7);
+                case userport_out( 6 downto 5) is
+                    when "10" =>
+                        -- joystick 0 (left jack)
+                        userport_reg( 1) <= not joystick_0( joy_right);
+                        userport_reg( 3) <= not joystick_0( joy_left);
+                        userport_reg( 2) <= not joystick_0( joy_up);
+                        userport_reg( 0) <= not joystick_0( joy_down);
+                        userport_reg( 4) <= not ( joystick_0( joy_fire_a) or joystick_0( joy_fire_b));
+                    when others =>
+                        -- joystick 1 (right jack)
+                        userport_reg( 1) <= not joystick_1( joy_right);
+                        userport_reg( 3) <= not joystick_1( joy_left);
+                        userport_reg( 2) <= not joystick_1( joy_up);
+                        userport_reg( 0) <= not joystick_1( joy_down);
+                        userport_reg( 4) <= not ( joystick_1( joy_fire_a) or joystick_1( joy_fire_b));
+                end case;
+                userport_reg( 5) <= userport_out( 5);
+                userport_reg( 6) <= userport_out( 6);
+                userport_reg( 7) <= userport_out( 7);
                 --
                 sound   <= userport_out( 7);
 
